b8b79d9cf01b9e280bec0d5ef61d015bd351a6ac
VladeMelo <vlademirjos@hotmail.com>
2020-08-25 08:47:18 -0300

    Adding preflight to cors config

--- src/server.js
+++ src/server.js
@@ -8,17 +8,21 @@ const SESMailProvider = require('./providers/SESMailProvider');
 const app = express();
 
 app.use(express.json());
-app.use((request, response, next) => {
-  response.header('Access-Control-Allow-Origin', '*');
-  response.header('Access-Control-Allow-Header', 
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
+app.use(cors({
+  preflightContinue: true,
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
+}))
+// app.use((request, response, next) => {
+//   response.header('Access-Control-Allow-Origin', '*');
+//   response.header('Access-Control-Allow-Header', 
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   )
 
-  if (request.method === 'OPTIONS') {
-    response.header('Access-Control-Allow-Methods', 'GET, POST')
-  }
-  next();
-});
+//   if (request.method === 'OPTIONS') {
+//     response.header('Access-Control-Allow-Methods', 'GET, POST')
+//   }
+//   next();
+// });
 
 app.post('/getting-email', async (request, response) => {
   const { hour, minute, date } = request.body;
